d07376a82cc1a4d7a7682d7edd2dd590566dba68
SignEd1366 <vovichcoolguy@gmail.com>
2025-01-08 17:10:42 +0200

    make homework 9

---
+++ src/js/1-gallery.js
@@ -0,0 +1,110 @@
+const images = [
+  {
+    id: 1,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    id: 2,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    id: 3,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    id: 4,
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    id: 5,
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    id: 6,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    id: 7,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    id: 8,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    id: 9,
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const createGalleryItemTemplate = item => {
+  return `
+  <li class="gallery-item" data-id="${item.id}">
+  <a class="gallery-link" href="${item.original}">
+    <img
+      class="gallery-image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>
+  `;
+};
+
+const galleryItemTemplate = images.map(el => createGalleryItemTemplate(el)).join('');
+
+const itemsListEl = document.querySelector('.gallery');
+
+itemsListEl.innerHTML = galleryItemTemplate;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const galleryLinks = document.querySelectorAll('.gallery-link');
+
+  galleryLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+    });
+  });
+});
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
---
+++ src/js/2-form.js
@@ -0,0 +1,62 @@
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+let formData = {
+    email: '',
+    message: '',
+};
+
+const fillFormFields = () => {
+    try {
+        if (localStorage.length === 0) {
+            return;
+        }
+
+        const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+        formData = formDataFromLS;
+
+        for (const key in formDataFromLS) {
+            feedbackFormEl.elements[key].value = formDataFromLS[key];
+        };
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+fillFormFields();
+
+const onFormFieldInput = event => {
+    const formFieldEl = event.target;
+
+    const fieldValue = formFieldEl.value;
+    const fieldName = formFieldEl.name;
+
+    formData[fieldName] = fieldValue;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+    event.preventDefault();
+
+    const { email, message } = formData;
+
+    if (!email || !message) {
+        alert('Fill please all fields');
+        return;
+    } else {
+        console.log(formData);
+    };
+
+    const { currentTarget: formEl } = event;
+
+    formData = { email: '', message: '' };
+    formEl.reset();
+    localStorage.removeItem('feedback-form-state');
+};
+
+feedbackFormEl.addEventListener('input', onFormFieldInput);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
+
+
